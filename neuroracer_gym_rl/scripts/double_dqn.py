f24c6ec4412761f93973a0ccc7e7c1fada5c03bb
Aray Karjauv <kararray@gmail.com>
2019-05-21 17:37:56 +0200

    double dqn agent added

--- neuroracer_gym_rl/scripts/double_dqn.py
+++ neuroracer_gym_rl/scripts/double_dqn.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
-
-from collections import deque
-import time
-import random
 import os
+import random
 
 import numpy as np
-import gym
-
 
 import keras
 from keras.models import Sequential,Input,Model
@@ -21,27 +15,28 @@ from keras.optimizers import Adam
 import rospy
 import rospkg
 
-from neuroracer_gym.tasks import neuroracer_discrete_task
+from utils import Memory
 
-      
 class Agent():
-    def __init__(self, state_size, action_size, always_explore=False):
+    def __init__(self, state_size, action_size, add_flipped=False, always_explore=False):
         rospack = rospkg.RosPack()
+        self.add_flipped = add_flipped
         self.always_explore = always_explore
         self.working_dir = rospack.get_path('neuroracer_gym_rl')
-        self.weight_backup      = os.path.join(self.working_dir, "double_dqn.h5")
+        self.weight_backup      = os.path.join(self.working_dir, "double_dqn_8f.h5")
 
         self.state_size         = state_size
         self.action_size        = action_size
         self.max_buffer         = 30000
         self.memory             = Memory(self.max_buffer)
         self.learning_rate      = 0.001
-        self.gamma              = 0.99
+        self.gamma              = 0.9
         self.exploration_rate   = 0.85
         self.exploration_min    = 0.01
         self.exploration_decay  = 0.99
         self.model              = self._build_model()
         self.target_model = self._build_model()
+        self.training_count = 0
 
 
     def _build_model(self):
@@ -91,7 +86,7 @@ class Agent():
 
 
     def save_model(self):
-        rospy.loginfo("Model saved"), 
+        rospy.loginfo("Model saved") 
         self.model.save(self.weight_backup)
 
     def act(self, state):
@@ -113,7 +108,7 @@ class Agent():
         
         return states_flipped, targets_flipped
 
-    def replay(self, new_data, add_flipped=True):
+    def replay(self, new_data):
 #         if self.memory.length() < sample_batch_size:
 #             return
 
@@ -138,141 +133,20 @@ class Agent():
         targets = self.model.predict(states)
         targets[np.arange(len(actions)), actions] = rewards_new
         
-        if add_flipped:
+        if self.add_flipped:
             states_flipped, targets_flipped = self.flip(actions, states, next_states, rewards, not_done)
             states = np.concatenate((states,states_flipped))
             targets = np.concatenate((targets,targets_flipped))
             
-        self.target_model.set_weights(self.model.get_weights())
-        
-        self.model.fit(states, targets, shuffle=True, batch_size=256, epochs=1, verbose=0)
+        self.model.fit(states, targets, shuffle=True, batch_size=32, epochs=1, verbose=0)
 
+        if self.training_count == 0 or self.training_count % 10 == 0:  
+            print('Updating weights')
+            self.target_model.set_weights(self.model.get_weights())
+        self.training_count+=1 
+        
         if self.exploration_rate > self.exploration_min:
             self.exploration_rate *= self.exploration_decay
 
         self.save_model()
-
-class NeuroRacer:
-    def __init__(self, always_explore=False):
-        self.sample_batch_size = 5000
-        self.episodes          = 100000
-        self.env               = gym.make('NeuroRacer-v0')
-
-        self.highest_reward    = -np.inf
         
-        self.n_frames = 4
-
-        self.img_y_offset = 200
-        self.img_y_scale = 0.2
-        self.img_x_scale = 0.2
-
-        state_size = self.env.observation_space.shape
-        self.state_size        = (int((state_size[0]-self.img_y_offset)*self.img_y_scale), int(state_size[1]*self.img_x_scale), 1)
-        rospy.loginfo("State size")
-        rospy.loginfo(self.state_size)
-
-        self.action_size       = self.env.action_space.n
-        self.agent             = Agent((self.state_size[0], self.state_size[1], self.state_size[2]*self.n_frames), self.action_size, always_explore=always_explore)
-
-        
-    def format_time(self, t):
-        m, s = divmod(int(time.time() - t), 60)
-        h, m = divmod(m, 60)
-        return "%d:%02d:%02d" % (h, m, s)
-    
-
-    
-    def run(self):
-        try:
-            total_time = time.time()
-            save_interval = 0
-            
-            memory = Memory()
-
-            for index_episode in range(self.episodes):
-                episode_time = time.time()
-
-                self.env.initial_position = {'p_x': np.random.uniform(1,4), 'p_y': 3.7, 'p_z': 0.05, 'o_x': 0, 'o_y': 0.0, 'o_z': np.random.uniform(0.4,1), 'o_w': 0.855}
-                state = self.env.reset()
-                state = self.preprocess(state)
-#                 state = np.expand_dims(state, axis=0)
-
-                done = False
-                cumulated_reward = 0
-                
-                stacked_states = deque(maxlen=self.n_frames)
-                stacked_next_states = deque(maxlen=self.n_frames)
-                for i in range(self.n_frames):
-                    stacked_states.append(state)
-                    stacked_next_states.append(state)
-                
-                steps = 0
-                while not done:
-                    steps+=1
-                    # if index_episode % 50 == 0:
-                    #     self.env.render()
-                    
-                    action = self.agent.act(np.expand_dims(np.stack(stacked_states, axis=2), axis=0))
-#                     action = self.agent.act(state)
-
-
-                    next_state, reward, done, _ = self.env.step(action)
-                    next_state = self.preprocess(next_state)
-#                     next_state = np.expand_dims(next_state, axis=0)
-                    stacked_next_states.append(next_state)
-
-                    memory.append(action,np.stack(stacked_states, axis=2), np.stack(stacked_next_states, axis=2), reward, done)        
-#                     self.agent.remember(history, action, reward, next_history, done)
-                    stacked_states.append(next_state)
-#                     self.agent.remember(state, action, reward, next_state, done)
-#                     state = next_state
-                    
-                    cumulated_reward += reward
-
-                    if save_interval >= self.sample_batch_size:
-                        save_interval = 0
-                        replay_time = time.time()
-                        self.agent.replay(memory)
-                        rospy.loginfo("Replay time {}".format(time.time()-replay_time))
-                        memory = Memory()
-                        # rospy.loginfo("Episode {} of {}. In-episode training".format(index_episode, self.episodes))
-                        # rospy.loginfo("Step {}, reward {}/{}".format(steps, cumulated_reward, self.highest_reward))
-                        # rospy.loginfo("Episode time {}, total {}".format(self.format_time(episode_time), 
-                        #                                                 self.format_time(total_time)))
-                    save_interval+=1
-                    
-
-                if self.highest_reward < cumulated_reward:
-                    self.highest_reward = cumulated_reward
-
-                rospy.loginfo("Episode {} of {}".format(index_episode, self.episodes))
-                rospy.loginfo("total steps {}, reward {}/{}".format(steps, cumulated_reward, self.highest_reward))
-                rospy.loginfo("Episode time {}, total {}".format(self.format_time(episode_time), 
-                                                                self.format_time(total_time)))
-                rospy.loginfo("exploration_rate {}".format(self.agent.exploration_rate))
-                
-                # self.agent.replay(self.sample_batch_size)
-                # if index_episode % 50 == 0:
-                #     self.env.close()
-        finally:
-            self.env.close()
-            # self.agent.save_model()
-            # print("Best score: " + str(self.best_score))
-
-if __name__ == '__main__':
-
-    rospy.init_node('neuroracer_qlearn', anonymous=True, log_level=rospy.INFO)
-
-    always_explore = rospy.get_param('/neuroracer_gym/always_explore')
-    # Create the Gym environment
-    game = NeuroRacer(always_explore=always_explore)
-    rospy.logwarn("Gym environment done. always_explore = " + str(always_explore))
-
-    # Set the logging system
-    # rospack = rospkg.RosPack()
-    # pkg_path = rospack.get_path('demo')
-    # outdir = pkg_path + '/training_results'
-    # rospy.loginfo("Monitor Wrapper started")
-
-    game.run()
-
