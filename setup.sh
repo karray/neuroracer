23dc361ea5c4f0cc824340c2c5488f04b164e9a1
Aray <kararray@gmail.com>
2019-05-31 17:41:37 +0200

    merge

--- setup.sh
+++ setup.sh
@@ -11,14 +11,15 @@ mkdir -p $CATKIN_SRC
 
 # apt-get update && apt-get install -yq --no-install-recommends apt-utils
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-
 apt-get update && apt-get install --no-install-recommends -yq git lsb-core curl wget xvfb \
     python-pip python-dev \
     dirmngr gnupg2 lsb-release
     #libgtk2.0-0 unzip libblas-dev liblapack-dev libhdf5-dev
 
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+apt-get update
 
 apt-get install -yq --no-install-recommends ros-melodic-desktop-full
 rosdep init
